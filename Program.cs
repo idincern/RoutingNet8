1853ef78431f9457ef83cebb819a92cbabc7ff20
idincern <dincernalbant@hotmail.com>
2024-05-08 11:54:00 +0300

    mapdefaultcontrollerroute() function and definition

--- Program.cs
+++ Program.cs
@@ -16,12 +16,30 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
+app.UseRouting(); // Gelen rotayı ayrıştırma işlemi yapan Middleware
 
 app.UseAuthorization();
 //dev branch
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}"); 
+
+#pragma warning disable ASP0014 // Suggest using top level route registrations
+var applicationBuilder = app.UseEndpoints(endpoints =>
+{
+    //Type 1(Default):
+    endpoints.MapDefaultControllerRoute(); // {controller=Home}/{action=Index}/{id?} = Default Route
+    // {}: Parameters. controller and action are pre-defined by the arhitecture. Other than these are custom parameters. Id is custom this case.
+    // If route 
+    //              / 
+    //              /home
+    //              /home/index
+    // is triggered, Home/Index will be returned by default.
+
+    // if the route is /personel/getir, then it will be triggered.
+    // if the route is only /personel, then /personel/index will be triggered.
+});
+#pragma warning restore ASP0014 // Suggest using top level route registrations
 
 app.Run();
